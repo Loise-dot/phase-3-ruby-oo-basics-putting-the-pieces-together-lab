1993c8bc85974a725ffc1719eb9cff6faa11a0d7
Loise <loise.wambui@student.moringaschool.com>
2023-02-13 19:44:53 +0300

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+require "pry"
+class Book 
+    attr_accessor :title
+    attr_accessor :author, :page_count, :genre
+    def initialize(title)
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+book = Book.new("And Then There Were None")
+puts book.turn_page
+#binding.pry
 
 
